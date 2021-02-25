5f817fda74909d3f0674ac3b84342462922d032e
Jasen <jasen.mackie@iress.com>
2021-02-25 14:12:04 -0500

    parallel processing with foreach

--- Iteration_1/Jasen_test.R
+++ Iteration_1/Jasen_test.R
@@ -1,39 +1,82 @@
 require(jsonlite)
 require(data.table)
-t1 <- Sys.time()
+require(foreach)
+require(doParallel)
 # Get data into a single list
 # Test with n files in a sub folder called test_data
-setwd("/home/jasen/Personal-Work/GitHub/MscProject")
-filenames <- list.files(path = "test_data")
+setwd("/home/jasen/Personal-Work/GitHub/MscProject/test_data")
+filenames <- list.files(path = "/home/jasen/Personal-Work/GitHub/MscProject/test_data")
 idx_bike <- list() # if we want to filter for "Bike" observations
-# for(f in 1:length(filenames)){
-#   idx_bike[[f]] <- which(fromJSON(txt=paste0("test_data/",filenames[f]))$RIDES$sport == "Bike")
+# t1 <- Sys.time()
+# for(f in 1:10){
+# # for(f in 520:530){
+#   if(f %% 10==0) { print(f)}
+#   try({
+#     idx_bike[[f]] <- which(fromJSON(txt=filenames[f])$RIDES$sport == "Bike" |
+#                              fromJSON(txt=filenames[f])$RIDES$sport == "VirtualRide")
+#   })
 # }
-# Build all the dataframes in a list called ls_metrics
-ls_metrics <- list()
-for(f in 1:length(filenames)){
-  ls_metrics[[f]] <- fromJSON(txt=paste0("test_data/",filenames[f]))$RIDES$METRICS#[idx_bike[[f]],]
-  ls_metrics[[f]][["date"]] <- fromJSON(txt=paste0("test_data/",filenames[f]))$RIDES$date#[idx_bike[[f]]]
-  ls_metrics[[f]][["id"]] <-  rep(fromJSON(txt=paste0("test_data/",filenames[f]))$ATHLETE$id, length(ls_metrics[[f]][["date"]]))
-  ls_metrics[[f]][["sport"]] <- fromJSON(txt=paste0("test_data/",filenames[f]))$RIDES$sport#[idx_bike[[f]]]
-  ls_metrics[[f]][["yob"]] <-  rep(fromJSON(txt=paste0("test_data/",filenames[f]))$ATHLETE$yob, length(ls_metrics[[f]][["date"]]))
-  ls_metrics[[f]][["gender"]] <-  rep(fromJSON(txt=paste0("test_data/",filenames[f]))$ATHLETE$gender, length(ls_metrics[[f]][["date"]]))
+# t2 <- Sys.time()
+# paste0("time to run bike index loop")
+# print(t2 - t1)
+
+registerDoParallel(cores = 8)
+# registerDoSEQ()
+t1 <- Sys.time()
+idx_bike <- foreach(f = 1:length(filenames), .combine = c, .multicombine = TRUE) %dopar% {
+  test <- try({
+    which(fromJSON(txt=filenames[f])$RIDES$sport == "Bike" |
+          fromJSON(txt=filenames[f])$RIDES$sport == "VirtualRide")
+  })
+  list(test)
 }
+t2 <- Sys.time()
+paste0("time to run bike index loop")
+print(t2 - t1)
+
+# Build all the dataframes in a list called ls_metrics
+# t1 <- Sys.time()
+dt_merged <- list()
+# for(f in 1:length(filenames)){
+#   if(f %% 10==0) { print(f)}
+#   try({
+#     dt_merged[[f]] <- fromJSON(txt=filenames[f])$RIDES$METRICS[idx_bike[[f]],]
+#     dt_merged[[f]][["date"]] <- fromJSON(txt=filenames[f])$RIDES$date[idx_bike[[f]]]
+#     dt_merged[[f]][["id"]] <-  rep(fromJSON(txt=filenames[f])$ATHLETE$id, length(dt_merged[[f]][["date"]]))
+#     dt_merged[[f]][["sport"]] <- fromJSON(txt=filenames[f])$RIDES$sport[idx_bike[[f]]]
+#     dt_merged[[f]][["yob"]] <-  rep(fromJSON(txt=filenames[f])$ATHLETE$yob, length(dt_merged[[f]][["date"]]))
+#     dt_merged[[f]][["gender"]] <-  rep(fromJSON(txt=filenames[f])$ATHLETE$gender, length(dt_merged[[f]][["date"]]))
+#   })
+# }
+
+dt_merged <- foreach(f = 1:length(filenames), .combine = c, .multicombine = TRUE) %dopar% {
+  test <- try({
+    dt_merged <- fromJSON(txt=filenames[f])$RIDES$METRICS[idx_bike[[f]],]
+    dt_merged[["date"]] <- fromJSON(txt=filenames[f])$RIDES$date[idx_bike[[f]]]
+    dt_merged[["id"]] <-  rep(fromJSON(txt=filenames[f])$ATHLETE$id, length(dt_merged[[f]][["date"]]))
+    dt_merged[["sport"]] <- fromJSON(txt=filenames[f])$RIDES$sport[idx_bike[[f]]]
+    dt_merged[["yob"]] <-  rep(fromJSON(txt=filenames[f])$ATHLETE$yob, length(dt_merged[[f]][["date"]]))
+    dt_merged[["gender"]] <-  rep(fromJSON(txt=filenames[f])$ATHLETE$gender, length(dt_merged[[f]][["date"]]))
+    })
+  list(dt_merged)
+}
+
+t3 <- Sys.time()
+paste0("time to run dt_merged loop")
+print(t3 - t2)
+
 # Merge into a data.table
-dt_merged <- rbindlist(ls_metrics, fill = TRUE)
+dt_merged <- rbindlist(dt_merged, fill = TRUE)
 # Reorder columns such that "id", "date", "sport", "yob" and "gender" are the first columns
 # setcolorder(dt_merged, c(colnames(idx_cols),colnames(dt_merged[-idx_cols]))) # this works, but only by reference...saves space but difficult to view
 idx_metadata_cols <- which(colnames(dt_merged) %in% c("date","id","sport","yob","gender"))
 idx_metric_cols <- which(!colnames(dt_merged) %in% c("date","id","sport","yob","gender"))
 dt_merged <- dt_merged[,c(idx_metadata_cols,idx_metric_cols), with=FALSE]
-bike <- dt_merged[sport=="Bike"]
-t2 <- Sys.time()
-print(t2-t1)
 
 idx_list <- which(sapply(dt_merged, class) == "list")
 idx_list
 
-class(type.convert(dt_merged[,11]))
+# class(type.convert(dt_merged[,11]))
 
 dt_merged <- dt_merged[,c('date','id','ride_count','workout_time','time_riding','athlete_weight','total_work','average_power','nonzero_power',
                           'max_power','cp_setting','coggan_np','coggan_if','coggan_tss','coggam_variability_index','coggan_tssperhour',
@@ -70,7 +113,11 @@ idx_list <- which(sapply(dt_merged, class)=="list")
 dt_merged$average_power <- as.numeric(unlist(lapply(dt_merged$average_power, `[[`, 1)))
 dt_merged$nonzero_power <- as.numeric(unlist(lapply(dt_merged$nonzero_power, `[[`, 1)))
 dt_merged$coggan_np <- as.numeric(unlist(lapply(dt_merged$coggan_np, `[[`, 1)))
+# remove rows with "NULL" values
+zero_idx <- which(dt_merged$coggan_tssperhour=="NULL")
+dt_merged$coggan_if[zero_idx] <- 0
 dt_merged$coggan_if <- as.numeric(unlist(lapply(dt_merged$coggan_if, `[[`, 1)))
+#
 dt_merged$coggam_variability_index <- as.numeric(unlist(lapply(dt_merged$coggam_variability_index, `[[`, 1)))
 # remove rows with "NULL" values
 zero_idx <- which(dt_merged$coggan_tssperhour=="NULL")
@@ -187,7 +234,7 @@ setnames(dt_merged, old=c('10m_peak_wpk'), new=c('peak_wpk_10m'))
 setnames(dt_merged, old=c('20m_peak_wpk'), new=c('peak_wpk_20m'))
 setnames(dt_merged, old=c('30m_peak_wpk'), new=c('peak_wpk_30m'))
 setnames(dt_merged, old=c('60m_peak_wpk'), new=c('peak_wpk_60m'))
-t2 <- Sys.time()
-print('Time to clean data')
-print(t2-t1)
+t4 <- Sys.time()
+print('Time to subset list elements, convert data types')
+print(t4-t3)
 
